fe2f59274efe5f25347dc140cc1c2ebde765cb3a
Sebastian <sebastian.morales01@uceva.edu.co>
2026-03-26 11:39:04 -0500

    feat: agregar nombre del estudiante y AppBar con variable de estado

--- taller1/lib/main.dart
+++ taller1/lib/main.dart
@@ -29,17 +29,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // variables de estado
+  String appBarTitle = 'Hola, Flutter';
+  bool isTitleChanged = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hola, Flutter'),
+        title: Text(appBarTitle),           // ← ahora usa la variable
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Pantalla inicial'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            const SizedBox(height: 16),
+            const Text(
+              'Sebastián Morales Flórez',    // ← nombre del estudiante
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
